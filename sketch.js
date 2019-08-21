8fea88b7caa57c0cd25c2b571b37b7f2597c8ef4
roxyrypler <30288569+roxyrypler@users.noreply.github.com>
2019-08-21 19:55:26 +0200

    now producing land masses

--- sketch.js
+++ sketch.js
@@ -3,14 +3,18 @@
 let cW = window.innerWidth;
 let cH = window.innerHeight;
 
-let tileSize = 40;
-let noiseScale = 0.02;
+let tileSize = 60;
+let noiseScale = 0.009;
+
+let water = "#4bcffa";
+let sand = "#ffdd59";
+let ground = "#485460";
 
 function setup() {
-    createCanvas(cW, cH);
-    background(0);
-    createTileGrid();
-    
+	createCanvas(cW, cH);
+	background(0);
+	createTileGrid();
+
 }
 
 function draw() {
@@ -19,17 +23,25 @@ function draw() {
 }
 
 function createTileGrid() {
-    for (var x = 0; x < width; x += width / tileSize) {
-        for (var y = 0; y < height; y += height / tileSize) {
-            fill(calculateNoise(x, y) * 255);
-            rect(x, y, width / tileSize, height / tileSize);
-            console.log(calculateNoise(x, y) * 255);
-        }
-    }
+	for (var x = 0; x < width; x += width / tileSize) {
+		for (var y = 0; y < height; y += height / tileSize) {
+			let tileChooser = calculateNoise(x, y);
+			if (tileChooser <= 0.4) {
+				fill(water);
+			} else if (tileChooser <= 0.6) {
+				fill(sand);
+			} else if (tileChooser <= 0.7) {
+				fill(ground);
+			}
+			noStroke();
+			rect(x, y, width / tileSize, height / tileSize);
+			console.log(calculateNoise(x, y) * 255);
+		}
+	}
 }
 
 function calculateNoise(x, y) {
-    noiseDetail(8, 0.65);
-    let noises = noise(x * noiseScale, y * noiseScale);
-    return noises;
+	noiseDetail(8, 0.5);
+	let noises = noise(x * noiseScale, y * noiseScale);
+	return noises;
 }
